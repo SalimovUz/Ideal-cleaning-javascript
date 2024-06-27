d002dedcf2bc9e0e7cd6d5114ec02447b7d6e837
SalimovUz <Salimofftolqin7@gmail.com>
2024-06-27 15:46:10 +0500

    Finally

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,167 +1,3 @@
-// import { TextField } from "@mui/material";
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { auth } from "../../service";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import InputAdornment from "@mui/material/InputAdornment";
-// import IconButton from "@mui/material/IconButton";
-// import { FaEye, FaEyeSlash } from "react-icons/fa";
-// import { Formik, Form } from "formik";
-// import * as Yup from "yup";
-
-// const Index = () => {
-//   const [showPassword, setShowPassword] = useState(false);
-//   const navigate = useNavigate();
-
-//   const validationSchema = Yup.object({
-//     email: Yup.string()
-//       .email("Email topilmadi!")
-//       .required("Email ni kiriting!"),
-//     password: Yup.string()
-//       .min(6, "Parol kamida 6 ta harf va raqamdan tashkil topishi kerak!")
-//       .matches(/[A-Z]/, "Parolda katta harf ham qatnashishi kerak!")
-//       .matches(/\d/, "Kamida bitta raqam ham bo'lishi shart!")
-//       .required("Parolni kiriting"),
-//   });
-
-//   const moveRegister = () => {
-//     navigate("/sign-up");
-//   };
-
-//   const handleClickShowPassword = () => {
-//     setShowPassword(!showPassword);
-//   };
-//   const handleMouseDownPassword = (event) => {
-//     event.preventDefault();
-//   };
-
-//   const handleSubmit = async (values, { setSubmitting }) => {
-//     try {
-//       const response = await auth.sign_in(values);
-
-//       if (response.status === 200) {
-//         localStorage.setItem("token", response?.data?.access_token);
-//         toast.success("Successfully Login!", {});
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       toast.error("Parol yoki Email xato kiritildi!");
-//     } finally {
-//       setSubmitting(false);
-//     }
-//   };
-
-//   return (
-//     <div className="flex h-screen items-center justify-center bg-gray-100">
-//       <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
-//         <h1 className="text-2xl font-semibold text-center text-gray-800">
-//           Sign-In
-//         </h1>
-//         <Formik
-//           initialValues={{
-//             email: "",
-//             password: "",
-//           }}
-//           validationSchema={validationSchema}
-//           onSubmit={handleSubmit}
-//         >
-//           {({
-//             values,
-//             handleChange,
-//             handleBlur,
-//             errors,
-//             touched,
-//             isSubmitting,
-//           }) => (
-//             <Form id="submit" className="mt-6 space-y-4">
-//               <TextField
-//                 fullWidth
-//                 label="Email"
-//                 name="email"
-//                 onChange={handleChange}
-//                 onBlur={handleBlur}
-//                 value={values.email}
-//                 type="text"
-//                 id="email"
-//                 className="my-2"
-//                 error={touched.email && Boolean(errors.email)}
-//                 helperText={touched.email && errors.email}
-//               />
-//               <div>
-//                 <TextField
-//                   fullWidth
-//                   label="Password"
-//                   name="password"
-//                   onChange={handleChange}
-//                   onBlur={handleBlur}
-//                   value={values.password}
-//                   type={showPassword ? "text" : "password"}
-//                   id="password"
-//                   required
-//                   InputProps={{
-//                     endAdornment: (
-//                       <InputAdornment position="end">
-//                         <IconButton
-//                           aria-label="toggle password visibility"
-//                           onClick={handleClickShowPassword}
-//                           onMouseDown={handleMouseDownPassword}
-//                         >
-//                           {showPassword ? <FaEyeSlash /> : <FaEye />}
-//                         </IconButton>
-//                       </InputAdornment>
-//                     ),
-//                   }}
-//                   error={touched.password && Boolean(errors.password)}
-//                   helperText={touched.password && errors.password}
-//                 />
-//               </div>
-//               <div className="text-center flex">
-//                 <a
-//                   onClick={moveRegister}
-//                   href="#"
-//                   className="text-blue-500 no-underline hover:underline"
-//                 >
-//                   <span className="text-black">
-//                     Agar sizda akkaunt bo'lmasa{" "}
-//                   </span>{" "}
-//                   Register
-//                 </a>
-//               </div>
-//               <div>
-//                 <a href="#">Parolni unitdingizmi?</a>
-//               </div>
-//               <div className="mt-2 text-center">
-//                 <button
-//                   type="submit"
-//                   className="px-4 py-2 flex  justify-center w-full text-center text-white bg-blue-500 rounded hover:bg-blue-600"
-//                   disabled={isSubmitting}
-//                 >
-//                   Sign In
-//                 </button>
-//               </div>
-//             </Form>
-//           )}
-//         </Formik>
-//         <ToastContainer
-//           position="top-right"
-//           autoClose={5000}
-//           hideProgressBar={false}
-//           newestOnTop={false}
-//           closeOnClick
-//           rtl={false}
-//           pauseOnFocusLoss
-//           draggable
-//           pauseOnHover
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Index;
-
-
 import {
   TextField,
   Dialog,
@@ -435,7 +271,7 @@ const Index = () => {
                   name="password"
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  value={values.newPassword}
+                  value={values.password}
                   type={showPassword ? "text" : "password"}
                   id="password"
                   required
@@ -452,8 +288,8 @@ const Index = () => {
                       </InputAdornment>
                     ),
                   }}
-                  error={touched.newPassword && Boolean(errors.newPassword)}
-                  helperText={touched.newPassword && errors.newPassword}
+                  error={touched.password && Boolean(errors.password)}
+                  helperText={touched.password && errors.password}
                 />
               </DialogContent>
               <DialogActions>
